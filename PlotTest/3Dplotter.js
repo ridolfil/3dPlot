1bfbab3802ada01f2adb63344a78ff5c308dd703
ridolfil <ridolfi.luc@gmail.com>
2015-01-23 09:57:11 +0000

    Small fixes

--- PlotTest/3Dplotter.js
+++ PlotTest/3Dplotter.js
@@ -5,15 +5,23 @@
 
         //Graph canvas
         var gr = this;
-        var graphMesh, wireMaterial, textMesh;
-        wireMaterial = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors, side: THREE.DoubleSide, wireframe: true, transparent: false });
 
         //Options
         var optDefault = {
-            bgColor: 0xFAF8DF
+            bgColor: 0xFAF8DF,
+            xOrig: -2.5,
+            zOrig: -2.5,
+            size: 5
         };
 
         var opt = $.extend(optDefault, options);
+
+        var x0 = opt.xOrig, z0 = opt.zOrig, size = opt.size;
+
+        var graphGeometry;
+
+        var labels = new Array();
+
 
         //Create a THREE Scene
         var scene = new THREE.Scene();
@@ -25,11 +33,8 @@
         var VIEW_ANGLE = 30,
             ASPECT = SCREEN_WIDTH / SCREEN_HEIGHT,
             NEAR = 0.1, FAR = 20000;
-
-        camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
-
-        camera.position.set(10, 4, 7);
-
+        var camera = new THREE.PerspectiveCamera(VIEW_ANGLE, ASPECT, NEAR, FAR);
+        camera.position.set(1.9*size, size, 1.2*size);
         scene.add(camera);
 
         //THREE Renderer
@@ -37,31 +42,16 @@
         var renderer = new THREE.WebGLRenderer({ antialias: true, alpha:true });
         //else
         //    renderer = new THREE.CanvasRenderer();
-
-        
         renderer.setClearColor(opt.bgColor, 1); // Set the background color
-
         renderer.setSize(SCREEN_WIDTH, SCREEN_HEIGHT);
         gr.append(renderer.domElement);
 
 
-        //Control
+        //Controls
         var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
+        controls.center.set(x0 + size / 2, z0 + size / 2, 0);
 
         //Set and create the grids
-        var gridXY = new THREE.GridHelper(2.5, 0.5);
-        gridXY.position.set(2.5, 0, 0);
-        gridXY.rotation.x = Math.PI / 2;
-        gridXY.setColors(new THREE.Color(0x000066), new THREE.Color(0xaaaaaa));
-        scene.add(gridXY);
-
-        var gridYZ = new THREE.GridHelper(2.5, 0.5);
-        gridYZ.position.set(0, 0, 2.5);
-        gridYZ.rotation.z = Math.PI / 2;
-        gridYZ.setColors(new THREE.Color(0x660000), new THREE.Color(0xaaaaaa));
-        scene.add(gridYZ);
-
 
         //Add the axis
         scene.add(new THREE.AxisHelper(5));
@@ -70,23 +60,27 @@
 
         createGraph();
 
-        var labels = new Array();
+
+
+        createGrids();
 
         createText();
+
         render();
 
 
 
         function createText() {
 
-            var pos;
-            for (pos = 2.5; pos >= -2.5; pos-=0.5)
-                addLabel(pos, new THREE.Vector3(5.1, pos, 0));
+            for (var pos = size/2; pos >= -size/2; pos-=size/10)
+                addLabel(pos, new THREE.Vector3(1.01*size+x0, pos, z0));
 
         }
      
         function addLabel(string, position) {
            
+            var textMesh;
+
             var textGeom = new THREE.TextGeometry(string,
             {
                 size: 0.17, height: 0.008, curveSegments: 3,
@@ -109,6 +103,7 @@
         function textLookAt() {
             for (var i = 0; i < labels.length; i++) labels[i].lookAt(camera.position);
         }
+
         //Add all the label numbers on the grid
         function addNumbers() { };
 
@@ -124,31 +119,30 @@
             renderer.render(scene, camera);
         };
 
-        function moveCamera() {
-            var x = camera.position.getComponent(0);
-            var y = camera.position.getComponent(1);
-            var z = camera.position.getComponent(2);
-            var l = Math.sqrt(x*x + z*z);
-            var ang = Math.atan(z / x) + 0.002;
-            console.log(ang);
-            camera.position.set(l*Math.cos(ang),y,l*Math.sin(ang) );
-        }
+
+        //function moveCamera() {
+        //    var x = camera.position.getComponent(0);
+        //    var y = camera.position.getComponent(1);
+        //    var z = camera.position.getComponent(2);
+        //    var l = Math.sqrt(x*x + z*z);
+        //    var ang = Math.atan(z / x) + 0.002;
+        //    console.log(ang);
+        //    camera.position.set(l*Math.cos(ang),y,l*Math.sin(ang) );
+        //}
 
 
         function createGraph() {
 
 
             var segments = 40;
-            var xRange = 5;
-            var yRange = 5;
             var meshFunction = function (x, y) {
-                x = xRange * x - 2.5;
-                y = yRange * y - 2.5;
+                x = size * x + x0;
+                y = size * y + z0;
                 var z = 0.5 * Math.sin(Math.sqrt(20 * x * x + 20 * y * y));//= Math.cos(x) * Math.sqrt(y);
                 if (isNaN(z))
                     return new THREE.Vector3(0, 0, 0); // TODO: better fix
                 else
-                    return new THREE.Vector3(x + 2.5, z, y + 2.5);
+                    return new THREE.Vector3(x, z, y);
             };
 
             // true => sensible image tile repeat...
@@ -157,6 +151,7 @@
             ///////////////////////////////////////////////
             // calculate vertex colors based on Z values //
             ///////////////////////////////////////////////
+
             graphGeometry.computeBoundingBox();
             var zMin = graphGeometry.boundingBox.min.y;
             var zMax = graphGeometry.boundingBox.max.y;
@@ -185,18 +180,37 @@
             ///////////////////////
 
             // material choices: vertexColorMaterial, wireMaterial , normMaterial , shadeMaterial
+            var wireMaterial = new THREE.MeshBasicMaterial({ vertexColors: THREE.VertexColors, side: THREE.DoubleSide, wireframe: true, transparent: false });
 
 
-            if (graphMesh) {
-                scene.remove(graphMesh);
-                 renderer.deallocateObject( graphMesh );
-            }
+            //if (graphMesh) {
+            //    scene.remove(graphMesh);
+            //     renderer.deallocateObject( graphMesh );
+            //}
 
-            //wireMaterial.map.repeat.set(segments, segments);
 
-            graphMesh = new THREE.Mesh(graphGeometry, wireMaterial);
+            var graphMesh = new THREE.Mesh(graphGeometry, wireMaterial);
             graphMesh.doubleSided = true;
             scene.add(graphMesh);
+        }
+
+        function createGrids() {
+
+            var step = size / 10;
+
+            var gridXY = new THREE.GridHelper(size/2, step);
+            gridXY.position.set(z0, 0, 0);
+            gridXY.rotation.z = Math.PI / 2;
+            gridXY.setColors(new THREE.Color(0x000066), new THREE.Color(0xaaaaaa));
+            scene.add(gridXY);
+
+            var gridYZ = new THREE.GridHelper(size/2, step);
+            gridYZ.position.set(0, 0, x0);
+            gridYZ.rotation.x = Math.PI / 2;
+            gridYZ.setColors(new THREE.Color(0x660000), new THREE.Color(0xaaaaaa));
+            scene.add(gridYZ);
+
+
         }
     };
 
